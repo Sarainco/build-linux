b998777ee72d5f2ac3a44130ea3a93c9934b8afe
yuji <yujibuzailai_sun@outlook.com>
2025-07-23 19:55:38 +0800

    add newchrdev

---
+++ kernel/newchrled/newchrled.c
@@ -0,0 +1,124 @@
+/**新字符设备驱动
+ * 1.字符设备结构；使用cdev结构体表示一个字符设备，主要是ops和dev
+ * 2.cdev_init函数
+ * 3.cdev_add函数；向linux添加字符设备
+ * 4.cdev_del函数；删除字符设备
+ * 5.自动创建节点
+ * 6.mdev机制：udev是一个用户程序，在linux下通过udev来实现设备文件的创建和删除，udev可以检测系统中的硬件设备状态，可以根据其状态来创建或删除设备文件
+ * 7.Linux系统中的热插拔事件也由mdev管理 echo /sbin/mdev > /proc/sys/kernel/hotplug
+ * 8.创建和删除类
+ * 9.创建设备
+ * 10.设置文件私有数据**/
+
+
+#include <linux/types.h>
+#include <linux/kernel.h>
+#include <linux/delay.h>
+#include <linux/ide.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/errno.h>
+#include <linux/gpio.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+
+#include <asm/mach/map.h>
+#include <asm/uaccess.h>
+#include <asm/io.h>
+
+#define NEWCHRLED_CNT			1		  	/* 设备号个数 */
+#define NEWCHRLED_NAME			"newchrled"	/* 名字 */
+#define LEDOFF 					0			/* 关灯 */
+#define LEDON 					1			/* 开灯 */
+
+struct newchrled_dev {
+    dev_t devid;    //设备号
+    struct cdev cdev;   //cdev
+    struct class *class;    //类
+    struct device *device;  //设备
+    int major;  //主设备号
+    int minor;  //次设备号
+};
+
+struct newchrled_dev newchrled;
+
+
+static int led_open(struct inode *inode, struct file *filp)
+{
+    return 0;
+}
+
+static int led_release(struct inode *inode, struct file *filp)
+{
+    return 0;
+}
+
+static ssize_t led_read(struct file *filp, char __user *buf, size_t cnt, loff_t *offt)
+{
+    return 0;
+}
+
+static ssize_t led_write(struct file *filp, const char __user *buf, size_t cnt, loff_t *offt)
+{
+    return 0;
+}
+
+static struct file_operations newchrled_fops = {
+    .owner = THIS_MODULE,
+    .open = led_open,
+    .release = led_release,
+    .read = led_read,
+    .write = led_write,
+};
+
+static int __init led_init(void)
+{
+    /*注册字符设备驱动*/
+    //1.创建设备号
+    if(newchrled.major) {
+        newchrled.devid = MKDEV(newchrled.major, 0);
+        register_chrdev_region(newchrled.devid, NEWCHRLED_CNT, NEWCHRLED_NAME);
+    } else {
+        alloc_chrdev_region(&newchrled.devid, 0, NEWCHRLED_CNT, NEWCHRLED_NAME);//申请设备号
+        newchrled.major = MAJOR(newchrled.devid);
+        newchrled.minor = MINOR(newchrled.devid);
+    }
+    printk("newcheled major=%d,minor=%d\r\n",newchrled.major, newchrled.minor);	
+
+    //2.初始化cdev
+    newchrled.cdev.owner = THIS_MODULE;
+    cdev_init(&newchrled.cdev, &newchrled_fops);
+
+    //3.添加一个cdev
+    cdev_add(&newchrled.cdev, newchrled.devid, NEWCHRLED_CNT);
+
+    //4.创建类
+    newchrled.class = class_create(THIS_MODULE, NEWCHRLED_NAME);
+    if(IS_ERR(newchrled.class)) {
+        return PTR_ERR(newchrled.class);
+    }
+
+    //5.创建设备
+    newchrled.device = device_create(newchrled.class, NULL, newchrled.devid, NULL, NEWCHRLED_NAME);
+	if (IS_ERR(newchrled.device)) {
+		return PTR_ERR(newchrled.device);
+	}
+
+    return 0;
+}
+
+static void __exit led_exit(void)
+{
+    //注销字符设备驱动
+    cdev_del(&newchrled.cdev); //删除cdev
+    unregister_chrdev_region(newchrled.devid, NEWCHRLED_CNT); //注销设备号
+
+    device_destory(newchrled.class, newchrled.devid);
+    class_destroy(newchrled.class);
+
+}
+
+mddule_init(led_init);
+module_exit(led_exit);
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("yuji");
