a87d931402524c77b7ec3c85c85261200235c235
yuji <yujibuzailai_sun@outlook.com>
2025-08-07 15:14:08 +0800

    delete coffee demo build

--- package/qml_demo/coffee/build/CMakeFiles/coffee.dir/build/.rcc/qrc_qml.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/coffee.dir/build/.rcc/qrc_qml.cpp.o: \
- /home/sunao/workspace/build-linux/package/qml_demo/coffee/build/.rcc/qrc_qml.cpp \
- /home/sunao/toolchain/toolchain-rk3568/aarch64-buildroot-linux-gnu/sysroot/usr/include/stdc-predef.h
--- package/qml_demo/coffee/build/CMakeFiles/coffee.dir/coffee_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/coffee.dir/coffee_autogen/mocs_compilation.cpp.o: \
- /home/sunao/workspace/build-linux/package/qml_demo/coffee/build/coffee_autogen/mocs_compilation.cpp \
- /home/sunao/toolchain/toolchain-rk3568/aarch64-buildroot-linux-gnu/sysroot/usr/include/stdc-predef.h
